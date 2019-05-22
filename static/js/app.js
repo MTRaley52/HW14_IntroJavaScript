e47fea754373fd014c4d0607177a72d306e0df05
Matt Raley <mtraley@gmail.com>
2019-05-21 23:59:06 -0500

    add

--- static/js/app.js
+++ static/js/app.js
@@ -38,15 +38,15 @@ data.forEach(function(ufoSights) {
 
   function myFunction() {
     var dataTable = d3.select("#ufo-table");
-    var tblBody = dataTable.select("#ufoSeen!");
+    var tblBody = dataTable.select("#ufoSeen");
     tblBody.html("");
     var date1 = document.getElementById("datetime").value;
     console.log(date1)
-    var ufosighted = document.getElementById("ufosighted");
+    var ufoSeen = document.getElementById("ufoSeen");
     for (var i = 0; i < data.length; i++) {
         if (data[i].datetime == date1) {
             var row = document.createElement("tr");
-            ufosighted.append(row);
+            ufoSeen.append(row);
             Object.entries(data[i]).forEach(([key, value]) => {
                 var cell = document.createElement("td");
                 cell.append(value);
